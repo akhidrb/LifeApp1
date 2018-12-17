3e8d9a6d11b8cfb5d8fe21f701e463d0d0b2b069
khidr <akhidr@bitbucket.org>
2018-12-17 22:03:54 +0200

    first commit

---
+++ src/main/java/com/example/LifeApp1/LifeApp1Application.java
@@ -0,0 +1,14 @@
+package com.example.LifeApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeApp1Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/LifeApp1/LifeApp1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.LifeApp1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LifeApp1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
